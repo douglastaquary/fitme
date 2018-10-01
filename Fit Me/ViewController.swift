559a2893149cd94d601c6a4e455a0fe9abd656bb
Douglas Taquary <douglas.taquary@resourceit.com>
2018-09-30 21:46:18 -0300

    Initial Commit

---
+++ Fit Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fit Me
+//
+//  Created by Douglas Taquary on 30/09/18.
+//  Copyright © 2018 Douglas Taquary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
