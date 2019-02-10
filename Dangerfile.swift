c52a8fb15e2e80ebdf9fd76926837cebfd0c9f6e
resource <resource@MacBook-Pro-de-resource.local>
2019-02-10 18:41:24 -0200

    update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,38 +1,14 @@
 import Danger
 import Foundation
-//import Yams 
+import Yams
 
  let danger = Danger()
- let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
- let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
- let sourceChange = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+ let modified = danger.git.modifiedFiles
 
-if !changelogChanged && sourceChange != nil {
-    warn("Something pretty bad, but not important enough to fail the build")
-}
+ let editedFiles = modified + danger.git.createdFiles //allSourceFiles.contains("CHANGELOG.md")
+ message("These files have changed: \(editedFiles.joined())")
 
- //Ensure the CHANGELOG is right
-//do {
-//    let changelogYML = danger.utils.readFile("CHANGELOG.yml")
-//    let loadedDictionary = try Yams.load(yaml: changelogYML) as? [String: Any]
-//    // So that we don't accidentally copy & paste wrong
-//    if let upcoming = loadedDictionary?["upcoming"] {
-//        if upcoming is Array<Any> {
-//            fail("Upcoming an array in the CHANGELOG")
-//        }
-//
-//        // Deployments rely on this, to send info to reviewers
-//        if upcoming is Dictionary<String, Any> {
-//            let upcomingDict = upcoming as! Dictionary<String, Any>
-//            if upcomingDict["user_facing"] == nil {
-//                fail("There must be a `user_facing` section in the upcoming section of the CHANGELOG")
-//            }
-//        }
-//    }
-//} catch {
-//    fail("The CHANGELOG is not valid YML")
-//}
 
-markdown("Free-form markdown that goes under the table, so you can do whatever.")
+ markdown("Free-form markdown that goes under the table, so you can do whatever.")
 
